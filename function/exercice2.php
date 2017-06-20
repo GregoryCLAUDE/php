73dfb2d1c3767aa17501d4073afd2b1dc5122d6f
Gregory <claudejurado@orange.fr>
2017-06-20 15:43:34 +0200

    rajout de loop

---
+++ function/exercice2.php
@@ -0,0 +1,9 @@
+<?php
+function string($string="yolo le monde"){
+  echo $string;
+
+}
+
+string();
+
+ ?>
---
+++ function/exercice3.php
@@ -0,0 +1,8 @@
+<?php
+function concat($s1="test de", $s2=" la concaténation"){
+
+  echo $s1, $s2;
+}
+
+concat();
+ ?>
