a6f55b876957f7d661c0851ddedf17ba08b0d37a
Gregory <claudejurado@orange.fr>
2017-06-22 16:02:48 +0200

    création de la base des formulaires

---
+++ form/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>inscription</title>
+  </head>
+  <body>
+    <form  action="user.php" method="post">
+      
+    </form>
+
+  </body>
+</html>
