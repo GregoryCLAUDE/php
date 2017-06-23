eeb13724332dbc2f14b92e951f7617a0b38691b8
Gregory <claudejurado@orange.fr>
2017-06-23 10:03:10 +0200

    exercices 1 et 2

--- form/index.php
+++ form/index.php
@@ -5,9 +5,17 @@
     <title>inscription</title>
   </head>
   <body>
-    <form  action="user.php" method="post">
-      
-    </form>
+    <form  action="user.php" method="get">
+        nom : <input type="text" name="nom" value="">
+        prenom : <input type="text" name="prenom" value="">
 
+        <button type="submit" name="button">envoyer en get</button>
+    </form>
+<form class="" action="user.php" method="post">
+    nom : <input type="text" name="name" value="">
+    prenom : <input type="text" name="firstname" value="">
+
+    <button type="submit" name="button">send in post</button>
+</form>
   </body>
 </html>
--- form/user.php
+++ form/user.php
@@ -5,6 +5,15 @@
     <title>page utilisateur</title>
   </head>
   <body>
-    
+   <?php
+   $name = $_GET["nom"];
+   $fname = $_GET["prenom"];
+   $nom = $_POST["name"];
+   $prenom = $_POST["firstname"];
+
+      echo "Bonjour ".$fname. " ". $name."."."<br>";
+      echo "Hello ". $nom. " " .$prenom."."."<br>";
+
+    ?>
   </body>
 </html>
