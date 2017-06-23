60ee010f76d95bd4a59d1f2cdd1d658c77c4a9f4
Gregory <claudejurado@orange.fr>
2017-06-23 10:06:07 +0200

    exercices 4 et 5

--- form/index.php
+++ form/index.php
@@ -6,16 +6,19 @@
   </head>
   <body>
     <form  action="user.php" method="get">
+      <p>Formulaire en Get</p>
         nom : <input type="text" name="nom" value="">
         prenom : <input type="text" name="prenom" value="">
 
-        <button type="submit" name="button">envoyer en get</button>
+        <button type="submit" name="button">envoyer</button>
     </form>
 <form class="" action="user.php" method="post">
+  <p>Form in post</p>
+  
     nom : <input type="text" name="name" value="">
     prenom : <input type="text" name="firstname" value="">
 
-    <button type="submit" name="button">send in post</button>
+    <button type="submit" name="button">send</button>
 </form>
   </body>
 </html>
